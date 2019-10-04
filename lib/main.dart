bb6ea35a89586e13d560cb29719ac391d75336be
daniel sarmiento cordero <danielsarcor@gmail.com>
2019-10-04 17:23:48 -0500

    Working with multiple providers

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,31 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: Products(),
-      child: MaterialApp(
-          title: 'Shop App',
-          theme: ThemeData(
-              primarySwatch: Colors.purple,
-              accentColor: Colors.deepOrange,
-              fontFamily: 'Lato'),
-          home: ProductsOverviewScreen(),
-          routes: {
-            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
-          }),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(
+            value: Products(),
+          ),
+          ChangeNotifierProvider.value(
+            value: Cart(),
+          )
+        ],
+        child: MaterialApp(
+            title: 'Shop App',
+            theme: ThemeData(
+                primarySwatch: Colors.purple,
+                accentColor: Colors.deepOrange,
+                fontFamily: 'Lato'),
+            home: ProductsOverviewScreen(),
+            routes: {
+              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
+            }));
   }
 }
